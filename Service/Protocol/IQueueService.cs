4bdf630a88f508f449e76be355031853d0bbb524
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-18 14:55:18 +0800

    建立 IQueueService
    1.　加入方法：Send
    2.　加入方法：Receive

---
+++ Service/Protocol/IQueueService.cs
@@ -0,0 +1,12 @@
+﻿using Model;
+using System;
+
+namespace Service.Protocol
+{
+    public interface IQueueService
+    {
+        void Send(Student student);
+
+        Student Receive(TimeSpan timeout);
+    }
+}
