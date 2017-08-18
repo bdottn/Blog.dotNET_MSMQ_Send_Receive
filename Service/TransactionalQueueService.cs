e207a06279791b16717aba27e238018c3e736c96
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-18 14:57:30 +0800

    建立 TransactionalQueueService
    1.　加入建構式：TransactionalQueueService
    　1.1　queuePath：訊息佇列用戶端存取名稱
    
    2.　繼承 IQueueService
    　2.1　實作方法：Send
    　2.2　實作方法：Receive

---
+++ Service/TransactionalQueueService.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using Service.Protocol;
+using System;
+using System.Messaging;
+
+namespace Service
+{
+    public sealed class TransactionalQueueService : IQueueService
+    {
+        private readonly MessageQueue messageQueue;
+        private readonly MessageQueueTransaction transaction;
+
+        public TransactionalQueueService(string queuePath)
+        {
+            this.messageQueue = new MessageQueue(queuePath);
+
+            this.transaction = new MessageQueueTransaction();
+        }
+
+        public void Send(Student student)
+        {
+            this.transaction.Begin();
+
+            this.messageQueue.Send(student, this.transaction);
+
+            this.transaction.Commit();
+        }
+
+        public Student Receive(TimeSpan timeout)
+        {
+            this.messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Student), });
+
+            this.transaction.Begin();
+
+            var message = this.messageQueue.Receive(timeout, this.transaction);
+
+            this.transaction.Commit();
+
+            return (Student)message.Body;
+        }
+    }
+}
