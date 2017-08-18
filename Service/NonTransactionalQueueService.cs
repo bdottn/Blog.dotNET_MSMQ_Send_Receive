6518ad25903d677c9881a51b010b1e310574714a
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-18 14:55:52 +0800

    建立 NonTransactionalQueueService
    1.　加入建構式：NonTransactionalQueueService
    　1.1　queuePath：訊息佇列用戶端存取名稱
    
    2.　繼承 IQueueService
    　2.1　實作方法：Send
    　2.2　實作方法：Receive

---
+++ Service/NonTransactionalQueueService.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using Service.Protocol;
+using System;
+using System.Messaging;
+
+namespace Service
+{
+    public sealed class NonTransactionalQueueService : IQueueService
+    {
+        private readonly MessageQueue messageQueue;
+
+        public NonTransactionalQueueService(string queuePath)
+        {
+            this.messageQueue = new MessageQueue(queuePath);
+        }
+
+        public void Send(Student student)
+        {
+            this.messageQueue.Send(student);
+        }
+
+        public Student Receive(TimeSpan timeout)
+        {
+            this.messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Student), });
+
+            var message = this.messageQueue.Receive(timeout);
+
+            return (Student)message.Body;
+        }
+    }
+}
